5308697808faeb6435d91a89f3c1821060544ab1
YavorMarkov <76599518+YavorMarkov@users.noreply.github.com>
2024-07-13 15:49:45 +0300

    new1

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 
-variable "project_id" {
-    description = "The Google Cloud project ID"
-    type        = string
-}
+# variable "project_id" {
+#     description = "The Google Cloud project ID"
+#     type        = string
+# }
 
-variable "region" {
-    description = "The region to host the Google Cloud resources"
-    type        = string
-}
+# variable "region" {
+#     description = "The region to host the Google Cloud resources"
+#     type        = string
+# }

f194d7dcacb01a6275f3d45771550ead33c890f8
YavorMarkov <76599518+YavorMarkov@users.noreply.github.com>
2024-07-13 17:08:30 +0300

    tttt

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "google_compute_instance" "free_vm" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
     }
   }
 

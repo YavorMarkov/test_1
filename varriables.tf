1a758197d5811e3e7352e0a7d86892be929bf49c
YavorMarkov <76599518+YavorMarkov@users.noreply.github.com>
2024-07-13 16:25:52 +0300

    newnew5

--- varriables.tf
+++ varriables.tf
@@ -1,9 +1,17 @@
+
+
 variable "project_id" {
     description = "The Google Cloud project ID"
     type        = string
 }
 
 variable "region" {
-    description = "The region for Google Cloud resources"
+    description = "The region to host the Google Cloud resources"
     type        = string
+}
+
+variable "GOOGLE_CREDENTIALS" {
+    description = "The JSON credentials for Google Cloud"
+    type        = string
+    sensitive   = true
 }
